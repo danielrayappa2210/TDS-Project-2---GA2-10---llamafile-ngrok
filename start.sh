fa8c2bffc7a162930481503483e99c1f84f9d93f
danielrayappa2210 <daniel.putta@gramener.org>
2025-03-13 16:20:31 +0000

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run llama-server container from Docker Hub
-docker run -d --name llama-container -p 9090:9090 your-dockerhub-username/llama-server:latest
+docker run -d --name llama-container -p 9090:9090 danielrayappa/llama-server:latest
 
 # Run ngrok to tunnel the connection
 ngrok authtoken $NGROK_AUTH_TOKEN
