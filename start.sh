b84767c91e81cda70170dedece77c22acb4f862d
danielrayappa2210 <daniel.putta@gramener.org>
2025-03-13 14:51:17 +0000

    Added start.sh

---
+++ start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Run Docker container in the background
+docker run -d --name llama-container -p 9090:9090 llama-server
+
+# Install ngrok (if needed)
+# wget https://bin.equinox.io/linux/amd64/ngrok-stable
+# tar -xvzf ngrok-stable
+# mv ngrok /usr/local/bin/
+
+# Run ngrok to tunnel the connection
+ngrok authtoken YOUR_NGROK_AUTH_TOKEN
+
+# Expose port 9090
+ngrok http 9090 --log="stdout" &
+
+# Get the ngrok URL
+sleep 5
+
+ngrok_url=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+if [[ -z "$ngrok_url" ]]; then
+    echo "Error: Could not retrieve ngrok URL."
+    exit 1
+fi
+
+echo "ngrok URL: $ngrok_url"
+
+# Keep the script running
+wait
